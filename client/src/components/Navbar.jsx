81959a9909046262a823267435e13806d5c32aa5
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-08-07 18:30:26 -0400

    more responsive design

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { UserMenu } from "../components";
+import { FaBars } from "react-icons/fa"; // Assuming you use this library
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
       <header className="navbar">
         <nav className="navbar-container">
-          <ul className="navbar-menu">
+          <button onClick={() => setIsOpen(!isOpen)} className="menu-icon">
+            <FaBars />
+          </button>
+          <ul className={isOpen ? "navbar-menu open" : "navbar-menu"}>
             <li>
-              <Link to="/mission">Mission</Link>
+              <Link to="/mission" onClick={() => setIsOpen(false)}>
+                Mission
+              </Link>
             </li>
             <li>
-              <Link to="/products">Products</Link>
+              <Link to="/products" onClick={() => setIsOpen(false)}>
+                Products
+              </Link>
             </li>
             <li>
-              <Link to="/testimonials">Testimonials</Link>
+              <Link to="/testimonials" onClick={() => setIsOpen(false)}>
+                Testimonials
+              </Link>
             </li>
             <UserMenu />
           </ul>
