49ea43c4cbf238f2d543ab32e64e3657fc7c33cb
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-08-07 12:23:43 -0400

    semantics

--- client/src/Pages/Products.jsx
+++ client/src/Pages/Products.jsx
@@ -8,18 +8,20 @@ export default function Products() {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div className="products-list">
-      {data.products.map(({ id, name, price, description, scents }) => (
-        <div key={id} className="product-container">
-          <h3 className="product-name">{name}</h3>
-          <b className="product-header"></b>
-          <p className="product-description">{description}</p>
-          <ul className="product-details">
-            <li className="product-scent">{scents}</li>
-            <li className="product-price">$ {price}</li>
-          </ul>
-        </div>
-      ))}
-    </div>
+    <>
+      <main className="products-list">
+        {data.products.map(({ id, name, price, description, scents }) => (
+          <section key={id} className="product-container">
+            <h3 className="product-name">{name}</h3>
+            <b className="product-header"></b>
+            <p className="product-description">{description}</p>
+            <ul className="product-details">
+              <li className="product-scent">{scents}</li>
+              <li className="product-price">$ {price}</li>
+            </ul>
+          </section>
+        ))}
+      </main>
+    </>
   );
 }
